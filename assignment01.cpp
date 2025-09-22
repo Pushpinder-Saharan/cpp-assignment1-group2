9c4142dfb7fe2f72eb8877e0fc7433306eb7a43a
Pushpinder Singh <pushpindersaharan5552@gmail.com>
2025-09-22 16:46:29 -0400

    Add files via upload
    Assignment01 .cpp file

---
+++ assignment01.cpp
@@ -0,0 +1,25 @@
+// Including required libraries to run code:
+#include <iostream>
+//The main entry point to run our program: main()
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    // For printing anything to the terminal: std::cout
+    // std::end1; to insert new line
+    // Printing 5 lines of text as per assignment requirement
+    std::cout << "Learning C++"<< std::endl;
+    std::cout << "Assignment No. 01" << std::endl;
+    std::cout << "Group No. 01" << std::endl;
+    std::cout << "Group Members: Johny Garg, Jashanpreet Singh, Pushpinder Singh" << std::endl;
+    //Security Topic requirements
+    std::cout << "Topic: Cloud Computing" << std::endl;
+    std::cout << "Cloud computing refers to the provision of computing services" << std::endl; 
+    //Using std::cout cmd multiple times to make output look like paragraph instead of a long line.
+    std::cout << "(servers, storage, databases, networking, software, and analytics) over the internet (the cloud) rather than on" << std::endl; 
+    std::cout << "local hardware or personal computers. It enables individuals and organizations to get hold of resources" << std::endl;
+    std::cout << "coutwhenever they require, scale resources up or down on an as-need basis and pay only a portion of what they consume." << std::endl;
+    std::cout << "This renders it economical, adaptable and economical. Cloud computing also allows remote access, collaboration and data backup," <<std::endl;
+    std::cout << "which can come in quite handy in the case of business and individuals who are willing to cut on infrastructure expenses" << std::endl;
+    std::cout << "be more productive." << std::endl;
+    return 0;
+    //return 0 to indicate that out program ended sucessfully
+}
